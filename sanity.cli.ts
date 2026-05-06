07971c9a3c0966bcf0205aa488b7b4a9b75ae141
Rafaa El Sherkasi <rafaa.el_sherkasi@sbb.ch>
2026-05-06 16:27:13 +0200

    sanity

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'hqx2k75n',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+    appId: 'wkhy2ql82qflja762mxw1b0v',
+  },
 })
